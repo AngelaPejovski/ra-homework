87158c767599b08ab9f34c3445b045606486e152
Angela11105 <angelapejovski7@gmail.com>
2022-05-13 09:48:23 +0200

    Exercise 2(b,c,d,e,f ) is done.

--- js-intro-homework/app2.js
+++ js-intro-homework/app2.js
@@ -4,3 +4,16 @@ let r = 23
 
 area = pi * r * r
 console.log(`the area of the cyrcle is ${area}`)
+
+// b.
+let c = 100;
+
+// d.
+r = c / 2*pi
+
+// e.
+D = 2*r
+
+// f.
+console.log(`The circle has a circumference of 
+${c}m. Its radius is ${r} and the diameter is ${D}.`)
